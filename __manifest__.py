1ddb365222dc93ea99329648fb1d67daff82894b
diegoalmonte-alpo <62121801+diegoalmonte-alpo@users.noreply.github.com>
2021-04-05 17:30:12 -0400

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# © 2018 Gustavo Valverde <gustavo@iterativo.do>
+# © 2018 Yasmany Castillo <yasmany003@gmail.com>
+# © 2018 José López <jlopez@indexa.do>
+
+# This file is part of NCF Manager.
+
+# NCF Manager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# NCF Manager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with NCF Manager.  If not, see <https://www.gnu.org/licenses/>.
+
+{
+    'name': "Labels para Facturas",
+    'summary': """
+    Añade Titulo de validez de NCF y Cuenta de Banco para Pago
+    """,
+    'author': "Diego Almonte, "
+              "Alpo SRL",
+    'license': 'LGPL-3',
+    'category': 'Localization',
+    'version': '12.0.2.1.0',
+    'depends': [],
+    'data': ['report/report_invoice.xml'],
+}
